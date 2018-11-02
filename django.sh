d80cfb5e571e199260087bb9531369acfb63cd0b
TannerHelms <tannerhelms01@gmail.com>
2018-11-02 15:47:23 -0600

    V-2

--- django.sh
+++ django.sh
@@ -1,124 +1,34 @@
 #!/bin/bash
-cd /home/compsci
+cd /home/$USER
 clear
-####################################
-#Create Directory
-####################################
 echo "What directory would you like to make?"
 read seldir
-
-####################################
-#Show the directory contents
-####################################
 echo "You have selected $seldir"
 ls | grep $seldir
-
-####################################
-#Name your project
-####################################
-
 echo "What would you like to name your project?"
 read answer
-
-
-
-####################################
-#clean
-####################################
-
+echo "What would you like to name your app?"
+read app
 sudo apt update
 sudo apt autoremove
-
-####################################
-#Clean and Start making the files!!!
-####################################
-
 cd /home/compsci
 mkdir $seldir
 cd $seldir 
-
-####################################
-#Start your project and runs the server
-####################################
-
+echo $app > myapp.txt
+echo $answer > projectname.txt
 echo "#!/bin/bash
 django-admin startproject $answer .
-tree
+python manage.py migrate
 python manage.py runserver" > server.sh
 sudo chmod 777 server.sh
-
 echo "#!/bin/bash
+python manage.py migrate
 python manage.py runserver" > runserver.sh
 sudo chmod 777 runserver.sh
-
-####################################
-#Creates Your App
-####################################
-
-cp /home/compsci/Desktop/CreateDjango/setup.sh startapp.sh
+cp /home/$USER/Desktop/DjangoCreaterV1-master/setup.sh startapp.sh
 sudo chmod 777 startapp.sh
-
-
-"""
-####################################
-#Views.py 
-####################################
-#START
-
-# pages/views.py
-from django.http import HttpResponse
-
-
-def homePageView(request):
-    return HttpResponse('Hello, World! This is Tanner')
-
-
-#END
-
-
-
-####################################
-# pages/urls.py
-####################################
-#START
-
-# pages/urls.py
-from django.urls import path
-
-from .views import homePageView
-
-urlpatterns = [
-    path('', homePageView, name='home')
-]
-
-#END
-####################################
-#urls.py /directoryname/project name
-####################################
-#START
-# helloworld_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-]
-#END
-
-"""
-
-####################################
-#Installs Django into your foler
-####################################
-
-
-
 pipenv install django==2.1
 echo "Now run ther server typing: ./server.sh"
 echo "Type ./startapp.sh to setup the app"
 echo "REMEMBER THAT THE NAME OF YOUR PROJECT IS :$answer"
 pipenv shell
-
-
-
--- setup.sh
+++ setup.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
-echo "What would you like to name your app?"
-read app
-echo "What was the name or your project?"
-read project
-echo
+app=$(cat myapp.txt)
+project=$(cat projectname.txt)
 python manage.py startapp $app
 cd $app
-echo "#START
 
+
+# /home/USR/FOLDER/$app/views.py
+
+
+echo "#START
 # pages/views.py
 from django.http import HttpResponse
-
-
 def homePageView(request):
     return HttpResponse('Hello, World! This is Tanner')
-
-
 #END" > views.py
-
 touch urls.py
 
-echo "#START
 
+# /home/USR/FOLDER/$app/urls.py
+
+
+echo "#START
 # pages/urls.py
 from django.urls import path
-
 from .views import homePageView
-
 urlpatterns = [
     path('', homePageView, name='home')
 ]
-
 #END" >urls.py
-
 cd ../$project
+
+
+# /home/USR/FOLDER/$project/urls.py
+
 
 echo "#START
 # helloworld_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('$app.urls')),
 ]
 #END" > urls.py
 
+
+# /home/USR/FOLDER/$project/settings.py
+
+
+
+
 cd ..
+python manage.py migrate
 python manage.py runserver
-
-
